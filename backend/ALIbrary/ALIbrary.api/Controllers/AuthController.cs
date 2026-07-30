6f79d3821686f50e9aa035944bae29430b7e0146
Ali Murtoza Shihab <67653116+AliMurtoza@users.noreply.github.com>
2026-07-30 23:38:50 +0600

    feat(auth): implement user registration with ASP.NET Identity

---
+++ backend/ALIbrary/ALIbrary.api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using ALIbrary.Application.Authentication.DTOs;
+using ALIbrary.Application.Authentication.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ALIbrary.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthController(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterRequest request)
+    {
+        var response = await _authenticationService.RegisterAsync(request);
+
+        return Ok(response);
+    }
+}
